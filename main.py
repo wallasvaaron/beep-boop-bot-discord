fa486d81a6f4a8615bcbb44b67b8bd408eefdce3
Aaron Wallasvaara <aaron.wallasvaara@live.fi>
2021-04-16 01:57:01 +0300

    testcommit22

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from discord import colour
 from discord.ext import commands
 from discord.utils import get
 import os
+import asyncio
 
+# loop = asyncio.get_event_loop()
+# future = asyncio.ensure_future(initialize())
+# loop.run_until_complete(future)
+print('check 1')
 
 async def botsays(ctx, input):
     await ctx.send(input) #, delete_after=20)
@@ -13,16 +18,22 @@ COMMAND_PREFIX = '?'
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 # guild = bot.get_guild(441310016100892682)
 
+print('check 2')
+
 @bot.event
 async def on_ready():
     # change_status.start()
     print(f'\nLogged in as: {bot.user.name}\n')
 
-list_of_cogs = []
+print('check 3')
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{str.lower(filename[:-3])}')
-        list_of_cogs.append(str.lower(filename[:-3]))
+list_of_cogs = []
+def initialize(): 
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{str.lower(filename[:-3])}')
+            list_of_cogs.append(str.lower(filename[:-3]))
+
+print('check 4')
 
 bot.run(os.environ['TOKEN'])
